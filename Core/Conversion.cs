8386e758afa38adc4cd50349eb5d58e9f44920c2
SpookyPool <51341995+SpookyPool@users.noreply.github.com>
2019-11-28 05:54:30 +0100

    Hex conversion

---
+++ Core/Conversion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Floweum_Node.Core
+{
+    class Conversion
+    {
+        public static string Hex(string text)
+        {
+            StringBuilder hex = new StringBuilder();
+            foreach (char c in text)
+            {
+                hex.Append(BitConverter.ToString(new byte[] { Convert.ToByte(c) }));
+            }
+            return hex.ToString();
+        }
+    }
+}
