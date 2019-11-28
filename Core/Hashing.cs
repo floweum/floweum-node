1910055ff1a68eedb0bbc4b35f1f0e7991471385
SpookyPool <51341995+SpookyPool@users.noreply.github.com>
2019-11-28 05:54:37 +0100

    Sha256 hasing

---
+++ Core/Hashing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Floweum_Node
+{
+    class Hashing
+    {
+        public static string Sha256(string randomString)
+        {
+            var crypt = new SHA256Managed();
+            string hash = String.Empty;
+            byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(randomString));
+            foreach (byte theByte in crypto)
+            {
+                hash += theByte.ToString("x2");
+            }
+            return hash;
+        }
+    }
+}
