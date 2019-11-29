a95b28cbfa85ee5c01267a61d0f0e2b2d7886fed
SpookyPool <51341995+SpookyPool@users.noreply.github.com>
2019-11-29 02:54:56 +0100

    Added Logo, API port. Changed 'CoinNetworkVersion' and 'SeedNode'

--- Core/Config.cs
+++ Core/Config.cs
@@ -1,13 +1,25 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 
 namespace Floweum_Node.Core
 {
     class Config
     {
+        public static void LogoText()
+        {
+            Console.WriteLine(@"  ______ _                                   ");
+            Console.WriteLine(@" |  ____| |                                  ");
+            Console.WriteLine(@" | |__  | | _____      _____ _   _ _ __ ___  ");
+            Console.WriteLine(@" |  __| | |/ _ \ \ /\ / / _ \ | | | '_ ` _ \ ");
+            Console.WriteLine(@" | |    | | (_) \ V  V /  __/ |_| | | | | | |");
+            Console.WriteLine(@" |_|    |_|\___/ \_/\_/ \___|\__,_|_| |_| |_|" + Environment.NewLine);
+            Console.WriteLine(@" v" + BuildVersion + " BUILD " + BuildName + Environment.NewLine);
+        }
+
         public static string CoinName                       = "Floweum";         // Coin name
         public static string CoinTicker                     = "FLOW";            // Coin ticker
         public static string CoinNetworkId                  = "FLW10NET";        // Network ID - Must be 8 chars
-        public static int    CoinNetworkVersion             = 0001;              // Network Version - Must be 4 chars
+        public static int    CoinNetworkVersion             = 1000;              // Network Version - Must be 4 chars
         public static string BuildVersion                   = Assembly.GetExecutingAssembly().GetName().Version.ToString();
         public static string BuildName                      = "RedAmazon";       // Build name
         /*public static int    DifficultTarget                = 600;               // 10 Minute block time
@@ -21,12 +33,14 @@ namespace Floweum_Node.Core
         public static long   GenesisBlockReward             = 5500000000000;     // 0,50% premine - 55000.00000000 FLOW - In atomic units
         public static string GenesisBlockWallet             = "FLOWajELTh4wpmLCCWTP3Ky02MH4D7Yx2Rc2YEEHLw4YGDo9p8OgIc7AMCtfAfAq";
         /*public static string WalletPrefix                   = "FLOW";            // Begin of the wallet address, 'FLOW...'
-        public static int    BlockUnlock                    = 3;                 // Block unlocked after 3 confirmations
-        public static int    Port                           = 4222;              // Port of the node */
+        public static int    BlockUnlock                    = 3;                 // Block unlocked after 3 confirmations */
+        public static int    Port                           = 4222;              // Port of the node
+        public static int    ApiPort                        = 4223;              // Port of the api
         public static string BlockchainTransactionsDatabase = "transactions.flowdb";
         public static string BlockchainBlocksDatabase       = "blocks.flowdb";
-        /*public static string[] SeedNodes = {
-            "127.0.0.1:4222"
-        };*/
+        public static string[] SeedNodes = {
+            "127.0.0.1",
+            "192.168.178.1"
+        };
     }
 }
