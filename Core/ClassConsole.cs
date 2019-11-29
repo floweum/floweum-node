73b0ef47fa59bc0b4b141ea313aa322be9343b42
SpookyPool <51341995+SpookyPool@users.noreply.github.com>
2019-11-29 02:56:28 +0100

    Added 't' key for testing

--- Core/ClassConsole.cs
+++ Core/ClassConsole.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Threading;
 using Floweum_Node.Core;
+using Floweum_Node.Node;
 
 namespace Floweum_Node
 {
@@ -11,14 +13,15 @@ namespace Floweum_Node
         public const string CommandHelp = "h";
         public const string CommandStatus = "s";
         public const string CommandExit = "e";
+        public const string CommandTest = "t";
 
     }
 
     public class ClassConsole
     {
-        public static void SetTitle()
+        public static void SetTitle(int connections)
         {
-            Console.Title = Config.CoinName + " v" + Config.BuildVersion + " " + Config.BuildName + " | Connections: 0";
+            Console.Title = Config.CoinName + " v" + Config.BuildVersion + " " + Config.BuildName + " | Connections: " + connections;
         }
 
         public static void CommandLine(string command)
@@ -26,32 +29,30 @@ namespace Floweum_Node
             switch (command.ToLower())
             {
                 case ConsoleKey.CommandHelp:
-                    ClassConsole.WriteLine("(" + ConsoleKey.CommandStatus + ") Show status - (" + ConsoleKey.CommandHelp + ") This help message - (" + ConsoleKey.CommandExit + ") Exit", ConsoleColor.Blue, false);
+                    ClassConsole.WriteLine("(" + ConsoleKey.CommandStatus + ") Show status - (" + ConsoleKey.CommandHelp + ") This help message - (" + ConsoleKey.CommandExit + ") Exit", "KEYCOMMAND", ConsoleColor.Blue, ConsoleColor.Blue);
+                    break;
+                case "t":
+                    NodeServer.sentToAll("test");
                     break;
                 case ConsoleKey.CommandExit:
                     Environment.Exit(0);
                     break;
             }
         }
-
-        public static void WriteLine(string text, ConsoleColor color, bool emptyTime)
+        
+        public static void WriteLine(string text, string type, ConsoleColor color, ConsoleColor color2)
         {
-            if (emptyTime)
-            {
-                int totalLenght = DateTime.Now.ToString().Length +5;
-                string totalEmpty = new String(' ', totalLenght);
-                Console.Write(totalEmpty);
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write("[" + DateTime.Now + "]");
-                Console.ForegroundColor = ConsoleColor.Gray;
-                Console.Write(" - ");
-            }
+            Thread.Sleep(100);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("[" + DateTime.Now + "] ");
+            Console.ForegroundColor = color2;
+            Console.Write("[" + type + "]");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(" - ");
             Console.ForegroundColor = color;
             Console.Write(text);
             Console.Write(Environment.NewLine);
+
         }
     }
 }
