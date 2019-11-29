c554f9d46f7c248cd081f87faa4cd6d8754d7e67
SpookyPool <51341995+SpookyPool@users.noreply.github.com>
2019-11-29 02:55:17 +0100

    Api Json Requests

---
+++ JsonParse/ApiError.cs
@@ -0,0 +1,8 @@
+﻿namespace Floweum_Node.API
+{
+    internal class ApiError
+    {
+        public int version { get; internal set; }
+        public string result { get; internal set; }
+    }
+}
---
+++ JsonParse/ApiStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace Floweum_Node.API
+{
+    internal class ApiStatus
+    {
+        public int version { get; internal set; }
+        public string result { get; internal set; }
+    }
+}
