b9637b0d88e7149b37c7655b82f9fa48750ace6a
SpookyPool <51341995+SpookyPool@users.noreply.github.com>
2019-12-22 22:52:56 +0100

    Added new colors

--- Core/Colors.cs
+++ Core/Colors.cs
@@ -8,14 +8,19 @@ namespace Floweum_Node.Core
     class Colors
     {
         public static Color DarkCyan = Color.FromArgb(51, 150, 160);
-        public static Color Cyan = Color.FromArgb(19, 161, 14);
-        public static Color DarkGreen = Color.FromArgb(19, 161, 14);
-        public static Color Green = Color.FromArgb(20, 198, 13);
+        public static Color Cyan = Color.FromArgb(133, 223, 223);
+
+        public static Color DarkGreen = Color.FromArgb(17, 142, 12);
+        public static Color Green = Color.FromArgb(21, 224, 14);
+
         public static Color DarkYellow = Color.FromArgb(193, 156, 0);
         public static Color Yellow = Color.FromArgb(249, 241, 165);
+
         public static Color Red = Color.FromArgb(255, 0, 0);
+
         public static Color DarkBlue = Color.FromArgb(0, 0, 255);
         public static Color Blue = Color.FromArgb(0, 0, 128);
+
         public static Color DarkMagenta = Color.FromArgb(136, 23, 152);
     }
 }
